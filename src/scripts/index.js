4f46bec2cca255fa588c6b9f49da1cb64a667e18
DESKTOP-1CFD9LR\Michal <adamsmichal@wp.pl>
2020-08-07 00:00:53 +0200

    Add mobile field and animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 const menuButton = document.querySelector(".mid-bar__burger-input");
+const searchButton = document.querySelector(".mid-bar__icon-container");
+const exitButton = document.querySelector(".mid-bar__form-exit");
 const nav = document.querySelector(".nav");
+const mobileForm = document.querySelector(".mid-bar__form-field");
+const mobileFormInput = document.querySelector(".mid-bar__form-input");
 
 const showMenu = () => {
   if (menuButton.checked) {
@@ -9,4 +13,20 @@ const showMenu = () => {
   }
 };
 
+const showMobileForm = () => {
+  mobileForm.style.height = "30vh";
+  mobileForm.style.padding = "30px";
+  mobileFormInput.style.display = "block";
+  exitButton.style.display = "block";
+};
+
+const closeMobileForm = () => {
+  mobileForm.style.height = "";
+  mobileForm.style.padding = "";
+  mobileFormInput.style.display = "";
+  exitButton.style.display = "";
+};
+
 menuButton.addEventListener("click", showMenu, false);
+searchButton.addEventListener("click", showMobileForm, false);
+exitButton.addEventListener("click", closeMobileForm, false);
